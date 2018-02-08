b4fc2f9ecc3bc4443889e8336e8edc3611bf8831
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2018-02-08 14:45:58 +0100

    Add copyright for featured images

--- twentysixteen-child/functions.php
+++ twentysixteen-child/functions.php
@@ -38,4 +38,45 @@ function add_my_post_types_to_query( $query ) {
 
 add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
 
+
+
+/**
+ * Add copyright for featured images.
+ *
+ * @since Twenty Sixteen Child 1.0
+ *
+ * @param string $html Post thumbnail HTML.
+ * @param int    $post_id Post ID.
+ * @param string $post_thumbnail_id Post thumbnail ID.
+ * @param string $size Post thumbnail size.
+ * @param string $attr Query string of attributes.
+ * @return string $html New post thumbnail HTML with copyright.
+ */
+
+function filter_post_thumbnail_html( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
+    if ( is_singular() ) {
+        $html = '<figure>' . $html;
+    } else {
+        $html = '<figure><a href="'. get_permalink($post_id). '" aria-hidden="true">' . $html . '</a>';
+    }
+
+    if (function_exists('get_featured_image_copyright')) {
+        $link = get_featured_image_copyright_link($post_thumbnail_id);
+        $author = get_featured_image_copyright_author($post_thumbnail_id);
+
+	if ( $link ) {
+                $html .= '<figcaption class="wp-post-image-copyright">&rsaquo; ';
+		$html .= '<a href="' . $link . '" target="_blank" rel="nofollow">';
+		$html .= $author . '</a></figcaption>';
+	}
+    }
+
+    $html .= '</figure>';
+
+    # Make filter magic happen here...
+    return $html;
+};
+
+add_filter( 'post_thumbnail_html', 'filter_post_thumbnail_html', 10, 5 );
+
 ?>
