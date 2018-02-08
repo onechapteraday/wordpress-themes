5bf6dff499e568c2e78131a0e47d3061f8504f67
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2018-02-08 14:38:06 +0100

    Add new custom post type book to queries

--- twentysixteen-child/functions.php
+++ twentysixteen-child/functions.php
@@ -16,4 +16,26 @@ function my_theme_enqueue_styles() {
     );
 }
 
+
+
+/**
+ * Add new custom post type book into queries.
+ *
+ * @since Twenty Sixteen Child 1.0
+ *
+ * @param array $query Query of the page.
+ * @return array A new modified query.
+ */
+
+function add_my_post_types_to_query( $query ) {
+    if ( !is_admin() ) {
+        if ( is_archive() && $query->is_main_query() )
+            $query->set( 'post_type', array( 'post', 'book' ) );
+    }
+
+    return $query;
+}
+
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+
 ?>
