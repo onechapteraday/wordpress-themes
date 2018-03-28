b12fd9e3f6e35108e4a3f78b2ec1a27db41a16c1
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2018-03-28 19:51:26 +0200

    Update entry_meta function to manage custom post types better

--- twentysixteen-child/functions.php
+++ twentysixteen-child/functions.php
@@ -210,17 +210,29 @@ if ( ! function_exists( 'twentysixteen_entry_meta' ) ) :
  * @since Twenty Sixteen Child 1.0
  */
 function twentysixteen_entry_meta() {
-    if ( 'post' === get_post_type() || 'book' === get_post_type() ) {
-        $author_avatar_size = apply_filters( 'twentysixteen_author_avatar_size', 49 );
-        printf( '<span class="byline"><span class="author vcard">%1$s<span class="screen-reader-text">%2$s </span> <a class="url fn n" href="%3$s">%4$s</a></span></span>',
-            get_avatar( get_the_author_meta( 'user_email' ), $author_avatar_size ),
-            _x( 'Author', 'Used before post author name.', 'twentysixteen' ),
-            esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-            get_the_author()
-        );
+    $post_types = array( 'post' );
+
+    if( post_type_exists( 'book' ) ){
+        array_push( $post_types, 'book' );
     }
 
-    if ( in_array( get_post_type(), array( 'post', 'book', 'attachment' ) ) ) {
+    if( post_type_exists( 'album' ) ){
+        array_push( $post_types, 'album' );
+    }
+
+    foreach( $post_types as $type ){
+        if( $type === get_post_type() ){
+            $author_avatar_size = apply_filters( 'twentysixteen_author_avatar_size', 49 );
+            printf( '<span class="byline"><span class="author vcard">%1$s<span class="screen-reader-text">%2$s </span> <a class="url fn n" href="%3$s">%4$s</a></span></span>',
+                get_avatar( get_the_author_meta( 'user_email' ), $author_avatar_size ),
+                _x( 'Author', 'Used before post author name.', 'twentysixteen' ),
+                esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+                get_the_author()
+            );
+        }
+    }
+
+    if ( in_array( get_post_type(), $post_types ) ) {
         twentysixteen_entry_date();
     }
 
@@ -233,8 +245,10 @@ function twentysixteen_entry_meta() {
         );
     }
 
-    if ( 'post' === get_post_type() || 'book' === get_post_type() ) {
-        twentysixteen_entry_taxonomies();
+    foreach( $post_types as $type ){
+        if( $type === get_post_type() ){
+            twentysixteen_entry_taxonomies();
+        }
     }
 
     if ( ! is_singular() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
