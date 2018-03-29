4d3a38e81874aa315e8ff6d02c124fb99a1acb1e
Christelle Hilaricus <christellehilaricus@users.noreply.github.com>
2018-03-29 11:13:59 +0200

    Add styles to admin

--- twentysixteen-child/functions.php
+++ twentysixteen-child/functions.php
@@ -1699,5 +1699,12 @@ add_filter('get_the_archive_title', function ($title) {
     return $title;
 });
 
+# Update CSS within in Admin
+
+function admin_style() {
+  wp_enqueue_style('admin-styles', get_stylesheet_directory_uri().'/admin.css');
+}
+
+add_action('admin_enqueue_scripts', 'admin_style');
 
 ?>
