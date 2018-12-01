4df0b7cd4a3a202716af818eee84ac586200cb7b
christellehilaricus <christellehilaricus@users.noreply.github.com>
2018-12-01 21:40:46 +0100

    Add for random posts widget the search by taxonomies

--- twentysixteen-child/widgets/random-posts-colored-widget.php
+++ twentysixteen-child/widgets/random-posts-colored-widget.php
@@ -18,17 +18,24 @@ class twentysixteenchild_randomposts_color extends WP_Widget {
     }
 
     public function widget($args, $instance) {
-        $title = isset($instance['title']) ? $instance['title'] : '';
+        $title      = isset($instance['title']) ? $instance['title'] : '';
         $postnumber = isset($instance['postnumber']) ? $instance['postnumber'] : '';
-        $category = isset($instance['category']) ? apply_filters('widget_title', $instance['category']) : '';
-        $tag = isset($instance['tag']) ? $instance['tag'] : '';
+        $category   = isset($instance['category']) ? apply_filters('widget_title', $instance['category']) : '';
+        $tag        = isset($instance['tag']) ? $instance['tag'] : '';
+        $publisher  = isset($instance['publisher']) ? $instance['publisher'] : '';
+        $location   = isset($instance['location']) ? $instance['location'] : '';
+        $person     = isset($instance['person']) ? $instance['person'] : '';
+        $prize      = isset($instance['prize']) ? $instance['prize'] : '';
 
         echo $args['before_widget'];
 
         if( ! empty( $title ) )
             echo '<div class="widget-title-wrap"><h3 class="widget-title"><span>'. esc_html($title) .'</span></h3></div>';
 
-        # The Query
+
+        ## The Query
+
+        # Add every existing post types
         $post_types = array( 'post' );
 
         if( post_type_exists( 'book' ) ){
@@ -43,15 +50,74 @@ class twentysixteenchild_randomposts_color extends WP_Widget {
             array_push( $post_types, 'interview' );
         }
 
-        $color_query = new WP_Query(array (
-            'post_status'         => 'publish',
-            'post_type'           => $post_types,
-            'posts_per_page'      => $postnumber,
-            'category_name'       => $category,
-            'tag'                 => $tag,
-            'orderby'             => 'rand',
-            'ignore_sticky_posts' => 1
-        ));
+        # Add custom taxonomy to WP_Query
+        $tax_query = array();
+
+        if( $publisher ){
+            $publisher_array = array(
+                'taxonomy' => 'publisher',
+                'field'    => 'slug',
+                'terms'    => $publisher,
+            );
+
+            array_push( $tax_query, $publisher_array );
+        }
+
+        if( $location ){
+            $location_array = array(
+                'taxonomy' => 'location',
+                'field'    => 'slug',
+                'terms'    => $location,
+            );
+
+            array_push( $tax_query, $location_array );
+        }
+
+        if( $person ){
+            $person_array = array(
+                'taxonomy' => 'person',
+                'field'    => 'slug',
+                'terms'    => $person,
+            );
+
+            array_push( $tax_query, $person_array );
+        }
+
+        if( $prize ){
+            $prize_array = array(
+                'taxonomy' => 'prize',
+                'field'    => 'slug',
+                'terms'    => $prize,
+            );
+
+            array_push( $tax_query, $prize_array );
+        }
+
+        # And compose
+        if( $tax_query ){
+            $color_query = new WP_Query(array (
+                'post_status'         => 'publish',
+                'post_type'           => $post_types,
+                'posts_per_page'      => $postnumber,
+                'category_name'       => $category,
+                'tag'                 => $tag,
+                'tax_query'           => $tax_query,
+                'orderby'             => 'rand',
+                'ignore_sticky_posts' => 1
+            ));
+        }
+        else {
+            $color_query = new WP_Query(array (
+                'post_status'         => 'publish',
+                'post_type'           => $post_types,
+                'posts_per_page'      => $postnumber,
+                'category_name'       => $category,
+                'tag'                 => $tag,
+                'orderby'             => 'rand',
+                'ignore_sticky_posts' => 1
+            ));
+        }
+
         ?>
 
         <div class="bg-wrap cf">
@@ -102,19 +168,27 @@ class twentysixteenchild_randomposts_color extends WP_Widget {
     }
 
     function update($new_instance, $old_instance) {
-        $instance['title'] = $new_instance['title'];
+        $instance['title']      = $new_instance['title'];
         $instance['postnumber'] = $new_instance['postnumber'];
-        $instance['category'] = $new_instance['category'];
-        $instance['tag'] = $new_instance['tag'];
+        $instance['category']   = $new_instance['category'];
+        $instance['tag']        = $new_instance['tag'];
+        $instance['publisher']  = $new_instance['publisher'];
+        $instance['location']   = $new_instance['location'];
+        $instance['person']     = $new_instance['person'];
+        $instance['prize']      = $new_instance['prize'];
 
         return $new_instance;
     }
 
     function form($instance) {
-        $title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+        $title      = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
         $postnumber = isset( $instance['postnumber'] ) ? esc_attr( $instance['postnumber'] ) : '';
-        $category = isset( $instance['category'] ) ? esc_attr( $instance['category'] ) : '';
-        $tag = isset( $instance['tag'] ) ? esc_attr( $instance['tag'] ) : '';
+        $category   = isset( $instance['category'] ) ? esc_attr( $instance['category'] ) : '';
+        $tag        = isset( $instance['tag'] ) ? esc_attr( $instance['tag'] ) : '';
+        $publisher  = isset( $instance['publisher'] ) ? esc_attr( $instance['publisher'] ) : '';
+        $location   = isset( $instance['location'] ) ? esc_attr( $instance['location'] ) : '';
+        $person     = isset( $instance['person'] ) ? esc_attr( $instance['person'] ) : '';
+        $prize      = isset( $instance['prize'] ) ? esc_attr( $instance['prize'] ) : '';
 
         ?>
 	<p>
@@ -135,6 +209,26 @@ class twentysixteenchild_randomposts_color extends WP_Widget {
 	<p>
 	    <label for="<?php echo $this->get_field_id('tag'); ?>"><?php _e('Tag slug (optional):','twentysixteen-child'); ?></label>
             <input type="text" name="<?php echo $this->get_field_name('tag'); ?>" value="<?php echo esc_attr($tag); ?>" class="widefat" id="<?php echo $this->get_field_id('tag'); ?>" />
+	</p>
+
+	<p>
+	    <label for="<?php echo $this->get_field_id('publisher'); ?>"><?php _e('Publisher slug (optional):','twentysixteen-child'); ?></label>
+            <input type="text" name="<?php echo $this->get_field_name('publisher'); ?>" value="<?php echo esc_attr($publisher); ?>" class="widefat" id="<?php echo $this->get_field_id('publisher'); ?>" />
+	</p>
+
+	<p>
+	    <label for="<?php echo $this->get_field_id('location'); ?>"><?php _e('Location slug (optional):','twentysixteen-child'); ?></label>
+            <input type="text" name="<?php echo $this->get_field_name('location'); ?>" value="<?php echo esc_attr($location); ?>" class="widefat" id="<?php echo $this->get_field_id('location'); ?>" />
+	</p>
+
+	<p>
+	    <label for="<?php echo $this->get_field_id('person'); ?>"><?php _e('Person slug (optional):','twentysixteen-child'); ?></label>
+            <input type="text" name="<?php echo $this->get_field_name('person'); ?>" value="<?php echo esc_attr($person); ?>" class="widefat" id="<?php echo $this->get_field_id('person'); ?>" />
+	</p>
+
+	<p>
+	    <label for="<?php echo $this->get_field_id('prize'); ?>"><?php _e('Prize slug (optional):','twentysixteen-child'); ?></label>
+            <input type="text" name="<?php echo $this->get_field_name('prize'); ?>" value="<?php echo esc_attr($prize); ?>" class="widefat" id="<?php echo $this->get_field_id('prize'); ?>" />
 	</p>
 	<?php
 
