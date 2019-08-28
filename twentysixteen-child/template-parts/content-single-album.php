ab90a5954c0dea4d302c4a250bf9ae66cdfc2315
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2019-08-28 12:57:25 -0400

    Update content single album

--- twentysixteen-child/template-parts/content-single-album.php
+++ twentysixteen-child/template-parts/content-single-album.php
@@ -26,27 +26,114 @@
 	<div class="entry-content">
 		<?php
 			the_content();
-		?>
-		<h2>À propos de cet album</h2>
+
+                        if( has_tag( 'single' ) ){
+		            ?>
+		            <h2><?php echo _x( 'About this single', 'single metadata section title', 'twentysixteen-child' ); ?></h2>
+                            <?php
+                        } else {
+		            ?>
+		            <h2><?php echo _x( 'About this album', 'album metadata section title', 'twentysixteen-child' ); ?></h2>
+                            <?php
+                        }
+                ?>
 		<table>
 			<?php
 			$album_id = get_the_ID();
 
-			$author = get_album_author( $album_id );
-			$author_second = get_album_author_second( $album_id );
+			$authors = get_album_author( $album_id );
 
-			if ( $author ) {
+			if( $authors ){
 			?>
 		        <tr>
 			        <td>
-					<b>Artiste<?php if ( $author_second ) { echo 's'; } ?></b>
+					<b><?php
+				        if( count( $authors ) > 1 ){
+					    $female_only = true;
+
+					    foreach( $authors as $author ){
+				                $gender = get_option( 'taxonomy_' . $author->term_id )['gender'];
+
+                                                if( $gender == 0 ){
+                                                    $female_only = false;
+                                                    break;
+                                                }
+					    }
+
+					    if( $female_only ){
+					        echo _x( 'Artists', 'album metadata female artists', 'twentysixteen-child' );
+					    } else {
+					        echo _x( 'Artists', 'album metadata artists', 'twentysixteen-child' );
+					    }
+					} else {
+				            $gender = get_option( 'taxonomy_' . $authors[0]->term_id )['gender'];
+
+					    if( $gender == 1 ){
+					        echo _x( 'Artist', 'album metadata female artist', 'twentysixteen-child' );
+					    } else {
+					        echo _x( 'Artist', 'album metadata male artist', 'twentysixteen-child' );
+					    }
+					}
+                                        ?></b>
 			        </td>
 			        <td>
 					<?php
-					echo '<a href="' . get_term_link( $author->term_id ). '">' . $author->name . '</a>';
+                                        $item_count = 0;
+				        foreach( $authors as $author ){
+					    if( $item_count > 0 ) echo ', ';
 
-					if ( $author_second ) {
-						echo ', <a href="' . get_term_link( $author_second->term_id ). '">' . $author_second->name . '</a>';
+					    echo '<a href="' . get_term_link( $author->term_id ) . '">' . $author->name . '</a>';
+                                            $item_count++;
+					}
+					?>
+			        </td>
+		        </tr>
+			<?php
+			}
+
+			$authors_featured = get_album_author_featured( $album_id );
+
+			if( $authors_featured ){
+			?>
+		        <tr>
+			        <td>
+					<b><?php
+				        if( count( $authors_featured ) > 1 ){
+					    $female_only = true;
+
+					    foreach( $authors_featured as $author ){
+				                $gender = get_option( 'taxonomy_' . $author->term_id )['gender'];
+
+                                                if( $gender == 0 ){
+                                                    $female_only = false;
+                                                    break;
+                                                }
+					    }
+
+					    if( $female_only ){
+					        echo _x( 'Featured artists', 'album metadata female featured artists', 'twentysixteen-child' );
+					    } else {
+					        echo _x( 'Featured artists', 'album metadata featured artists', 'twentysixteen-child' );
+					    }
+					} else {
+				            $gender = get_option( 'taxonomy_' . $authors_featured[0]->term_id )['gender'];
+
+					    if( $gender == 1 ){
+					        echo _x( 'Featured artist', 'album metadata female featured artist', 'twentysixteen-child' );
+					    } else {
+					        echo _x( 'Featured artist', 'album metadata male featured artist', 'twentysixteen-child' );
+					    }
+					}
+                                        ?></b>
+			        </td>
+			        <td>
+					<?php
+                                        $item_count = 0;
+				        foreach( $authors_featured as $author ){
+					    if( $item_count > 0 ) echo ', ';
+
+					    echo '<a href="' . get_term_link( $author->term_id ) . '">' . $author->name . '</a>';
+                                            $item_count++;
 					}
 					?>
 			        </td>
@@ -56,21 +143,27 @@
 			?>
 		        <tr>
 			        <td>
-					<b>Titre</b>
+					<b><?php echo _x( 'Title', 'album metadata title', 'twentysixteen-child' ); ?></b>
 			        </td>
 			        <td>
 					<?php echo get_album_title_original( $album_id ); ?>
 			        </td>
 		        </tr>
-		        <tr>
-			        <td>
-					<b>Parution</b>
-			        </td>
-			        <td>
-					<?php echo date_i18n( 'j F Y', strtotime(get_album_date_release( $album_id )) ); ?>
-			        </td>
-		        </tr>
 			<?php
+
+                        if( get_album_date_release( $album_id ) ){
+
+			        ?>
+		                <tr>
+			                <td>
+                                                <b><?php echo _x( 'Release date', 'album metadata release date', 'twentysixteen-child' ); ?></b>
+			                </td>
+			                <td>
+                                                <?php echo date_i18n( 'j F Y', strtotime(get_album_date_release( $album_id )) ); ?>
+			                </td>
+		                </tr>
+			        <?php
+			}
 
 			$price = get_album_price( $album_id );
 
@@ -79,7 +172,7 @@
 			?>
 		        <tr>
 			        <td>
-					<b>Prix</b>
+					<b><?php echo _x( 'Price', 'album metadata price', 'twentysixteen-child' ); ?></b>
 			        </td>
 			        <td>
 					<?php echo $price; ?> &euro;
@@ -96,12 +189,12 @@
 			?>
 		        <tr>
 			        <td>
-					<b>Lien affilié</b>
+					<b><?php echo _x( 'Affiliate link', 'album metadata Amazon affiliate link', 'twentysixteen-child' ); ?></b>
 			        </td>
 			        <td>
 					<a href="<?php echo $amazon['link']; ?>" target="_blank" rel="nofollow" class="logo_partner logo_amazon">
-				            <img src="<?php echo $amazon['img_buy']; ?>" alt="Achat sur Amazon" />
-                                            <span>Acheter sur Amazon</span>
+				            <img src="<?php echo $amazon['img_buy']; ?>" alt="Amazon" />
+                                            <span><?php echo _x( 'Buy on Amazon', 'album metadata Amazon affiliate message', 'twentysixteen-child' ); ?></span>
 					</a>
 			        </td>
 		        </tr>
