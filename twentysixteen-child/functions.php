cc2a0971bec20651f741682102af597fe111a80a
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2020-01-26 18:21:00 -0500

    Add new random small widgets

--- twentysixteen-child/functions.php
+++ twentysixteen-child/functions.php
@@ -586,6 +586,8 @@ add_action( 'widgets_init', 'twentysixteenchild_widgets_init' );
 
 # Import widgets
 
+require_once( __DIR__ . '/widgets/random-posts-small-one-widget.php');
+require_once( __DIR__ . '/widgets/random-posts-small-two-widget.php');
 require_once( __DIR__ . '/widgets/random-posts-medium-one-widget.php');
 require_once( __DIR__ . '/widgets/random-posts-medium-two-widget.php');
 require_once( __DIR__ . '/widgets/random-posts-big-one-widget.php');
