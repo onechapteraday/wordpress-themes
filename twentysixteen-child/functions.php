1ea69740b7bee32e32f5879157540b9f6f902f23
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2020-03-30 17:56:12 -0400

    Split CSS to reduce page speed

--- twentysixteen-child/functions.php
+++ twentysixteen-child/functions.php
@@ -14,6 +14,22 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
+    if( is_home() || is_front_page() ){
+        wp_enqueue_style( 'child-widgets-style',
+            get_stylesheet_directory_uri() . '/css/widgets.css',
+            array( $parent_style ),
+            wp_get_theme()->get('Version')
+        );
+    }
+
+    if( is_single() ){
+        wp_enqueue_style( 'child-single-style',
+            get_stylesheet_directory_uri() . '/css/single.css',
+            array( $parent_style ),
+            wp_get_theme()->get('Version')
+        );
+    }
 }
 
 
@@ -929,5 +945,46 @@ function wps_deregister_styles() {
 }
 
 add_action( 'wp_print_styles', 'wps_deregister_styles', 100 );
+
+
+/**
+ * Remove Jetpack CSS
+ */
+
+# First, make sure Jetpack doesn't concatenate all its CSS
+
+add_filter( 'jetpack_implode_frontend_css', '__return_false' );
+
+# Then, remove each CSS file, one at a time
+
+function jeherve_remove_all_jp_css() {
+  wp_deregister_style( 'AtD_style' );                    # After the Deadline
+  wp_deregister_style( 'jetpack_likes' );                # Likes
+  wp_deregister_style( 'jetpack_related-posts' );        # Related Posts
+  wp_deregister_style( 'jetpack-carousel' );             # Carousel
+  wp_deregister_style( 'grunion.css' );                  # Grunion contact form
+  wp_deregister_style( 'the-neverending-homepage' );     # Infinite Scroll
+  wp_deregister_style( 'infinity-twentyten' );           # Infinite Scroll - Twentyten Theme
+  wp_deregister_style( 'infinity-twentyeleven' );        # Infinite Scroll - Twentyeleven Theme
+  wp_deregister_style( 'infinity-twentytwelve' );        # Infinite Scroll - Twentytwelve Theme
+  wp_deregister_style( 'noticons' );                     # Notes
+  wp_deregister_style( 'post-by-email' );                # Post by Email
+  wp_deregister_style( 'publicize' );                    # Publicize
+  wp_deregister_style( 'sharedaddy' );                   # Sharedaddy
+  wp_deregister_style( 'sharing' );                      # Sharedaddy Sharing
+  wp_deregister_style( 'jetpack-widgets' );              # Widgets
+  wp_deregister_style( 'jetpack-slideshow' );            # Slideshows
+  wp_deregister_style( 'presentations' );                # Presentation shortcode
+  wp_deregister_style( 'jetpack-subscriptions' );        # Subscriptions
+  wp_deregister_style( 'widget-conditions' );            # Widget Visibility
+  wp_deregister_style( 'jetpack_display_posts_widget' ); # Display Posts Widget
+  wp_deregister_style( 'gravatar-profile-widget' );      # Gravatar Widget
+  wp_deregister_style( 'widget-grid-and-list' );         # Top Posts widget
+  wp_deregister_style( 'jetpack-widgets' );              # Widgets
+  #wp_deregister_style( 'stats_reports_css' );           # Stats
+  #wp_deregister_style( 'tiled-gallery' );               # Tiled Galleries
+}
+
+add_action('wp_print_styles', 'jeherve_remove_all_jp_css' );
 
 ?>
