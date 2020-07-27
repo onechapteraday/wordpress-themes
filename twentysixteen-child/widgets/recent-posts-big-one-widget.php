fa9677dc852a9053d58f646a4bc5ddaf0a02f9ed
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2020-07-26 23:28:57 -0400

    Add quick fix to get good image in wide screens
    - TODO: Implement a better use of wp_calculate_image_sizes

--- twentysixteen-child/widgets/recent-posts-big-one-widget.php
+++ twentysixteen-child/widgets/recent-posts-big-one-widget.php
@@ -205,7 +205,11 @@ class twentysixteenchild_recentposts_big_one extends WP_Widget {
 
                     <?php if ( '' != get_the_post_thumbnail() ) : ?>
                         <div class="entry-thumb">
-                            <?php twentysixteen_post_thumbnail( 'twentysixteenchild-fullwidth', true ); ?>
+                            <?php
+                            add_filter( 'wp_calculate_image_srcset_meta', '__return_null' );
+                            twentysixteen_post_thumbnail( 'twentysixteenchild-fullwidth', true );
+                            remove_filter( 'wp_calculate_image_srcset_meta', '__return_null' );
+                            ?>
                         </div><!-- end .entry-thumb -->
                     <?php endif; ?>
 
