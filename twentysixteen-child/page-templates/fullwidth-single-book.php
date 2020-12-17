55b2ad01fde7d74966fcb24b69c054f019989219
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2020-12-17 18:43:22 -0500

    Fix book navigation links

--- twentysixteen-child/page-templates/fullwidth-single-book.php
+++ twentysixteen-child/page-templates/fullwidth-single-book.php
@@ -30,7 +30,7 @@ get_header(); ?>
 				the_post_navigation( array(
 					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
 				) );
-			} elseif ( is_singular( 'post' ) ) {
+			} elseif ( is_singular( 'book' ) ) {
 				// Previous/next post navigation.
 				the_post_navigation( array(
 					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
