2048fce56e505741b75a0185353ce230516a0f2f
onechapteraday <13764558+onechapteraday@users.noreply.github.com>
2021-06-22 17:41:55 -0400

    Order probably collections name

--- twentysixteen-child/taxonomy-publisher.php
+++ twentysixteen-child/taxonomy-publisher.php
@@ -58,15 +58,7 @@ get_header(); ?>
                                         }
 
                                         if( $collections_with ){
-                                            function sort_collection_by_name( $a, $b ){
-                                                $translit = array('Á'=>'A','À'=>'A','Â'=>'A','Ä'=>'A','Ã'=>'A','Å'=>'A','Ç'=>'C','É'=>'E','È'=>'E','Ê'=>'E','Ë'=>'E','Í'=>'I','Ï'=>'I','Î'=>'I','Ì'=>'I','Ñ'=>'N','Ó'=>'O','Ò'=>'O','Ô'=>'O','Ö'=>'O','Õ'=>'O','Ú'=>'U','Ù'=>'U','Û'=>'U','Ü'=>'U','Ý'=>'Y','á'=>'a','à'=>'a','â'=>'a','ä'=>'a','ã'=>'a','å'=>'a','ç'=>'c','é'=>'e','è'=>'e','ê'=>'e','ë'=>'e','í'=>'i','ì'=>'i','î'=>'i','ï'=>'i','ñ'=>'n','ó'=>'o','ò'=>'o','ô'=>'o','ö'=>'o','õ'=>'o','ú'=>'u','ù'=>'u','û'=>'u','ü'=>'u','ý'=>'y','ÿ'=>'y');
-                                                $at = strtolower( strtr( $a->name, $translit ) );
-                                                $bt = strtolower( strtr( $b->name, $translit ) );
-
-                                                return strcoll( $at, $bt );
-                                            }
-
-                                            usort( $collections, 'sort_collection_by_name' );
+                                            usort( $collections_with, 'sortByCollectionName' );
 
 				            echo '<div class="taxonomy-description publisher-collections">';
 				            echo '<p><u>' . __( 'Collections', 'twentysixteen-child' ) . '</u>' . __( ': ', 'twentysixteen-child' );
